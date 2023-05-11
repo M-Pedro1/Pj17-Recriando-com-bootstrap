0f120df9381fb728088c26e2e8e92a4fa858228a
Pedro <mario.coctop@gmail.com>
2023-05-10 22:44:00 -0300

    v1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function loginFace(){
+    window.location.href="https://www.facebook.com"
+}
+
+function loginGoogle(){
+    window.location.href="https://accounts.google.com/v3/signin/identifier?dsh=S-838596638%3A1683768360200032&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&hl=pt-BR&ifkv=Af_xneGiZ8-Y7rA8ymh6V8xUH5rW89X5JVKexQHaV56O7m1t0uu11NnC05kKYgR31arRizG9nH5K&passive=true&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
+}
